334c262fff294a097211260a08002cb5202e32cd
VHugoBarnes <hugovg799@gmail.com>
2019-04-28 18:25:59 -0500

    Initial commit

---
+++ src/com/victorhugo/jugadores/Main.java
@@ -0,0 +1,9 @@
+package com.victorhugo.jugadores;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/com/victorhugo/jugadores/visual/MainFrame.java
@@ -0,0 +1,21 @@
+package com.victorhugo.jugadores.visual;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    /***/
+    public MainFrame(){
+
+    }
+
+    /**
+     * Clase privada PanelQuery, creada para almacenar los
+     * */
+    private class PanelQuery{
+
+    }
+
+    JLabel titulo, etiqueta1, etiqueta2;
+
+}
